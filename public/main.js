72fa3b61b28df0be0f1ea5828a9ac811817faa1d
bharath <bharath@email.com>
2022-05-15 14:43:25 +0530

    Place marker at lat long

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
+function latLongToXYZ(geoLoc)
+{
+        let pos = {x:0, y:0, z:0};
+        let geoLoc_radian = {lat: 0, long: 0}
+        for(var coordinate in geoLoc)
+        {
+                geoLoc_radian[coordinate] =  geoLoc[coordinate] * Math.PI/180
+                geoLoc_radian[coordinate] *= -1 // Correction due to inverted texture wrap
+        }
+        pos.x = Math.cos(geoLoc_radian.long)
+        pos.y = Math.sin(geoLoc_radian.lat) * Math.sin(geoLoc_radian.long)
+        pos.z = Math.cos(geoLoc_radian.lat) * Math.sin(geoLoc_radian.long)
+        return pos
+}
 var scene = new THREE.Scene()
 
 const earth_texture = new THREE.TextureLoader().load('textures/earth.jpg');
@@ -5,9 +19,19 @@ const sphereGeom = new THREE.SphereGeometry(1, 32, 16)
 const sphereMaterial = new THREE.MeshBasicMaterial({map: earth_texture})
 const sphere = new THREE.Mesh(sphereGeom, sphereMaterial)
 sphere.position.x = 0
-sphere.position.x = 0
-sphere.position.x = 0
+sphere.position.y = 0
+sphere.position.z = 0
 sphere.castShadow = true;
+
+const markerGeom = new THREE.SphereGeometry(0.025, 32, 16)
+const markerMaterial = new THREE.MeshBasicMaterial({color: 'red'})
+const marker = new THREE.Mesh(markerGeom, markerMaterial)
+
+pos = latLongToXYZ({lat: -25.27, long: 133.77})
+
+marker.position.x = pos.x
+marker.position.y = pos.y
+marker.position.z = pos.z
 
 var pointLight = new THREE.PointLight(0xffffff, 0.4)
 pointLight.castShadow = true;
@@ -24,6 +48,7 @@ gui.add(pointLight, 'intensity', 0, 5)
 
 scene.add(pointLight)
 scene.add(sphere)
+scene.add(marker)
 
 
 var camera = new THREE.PerspectiveCamera(
